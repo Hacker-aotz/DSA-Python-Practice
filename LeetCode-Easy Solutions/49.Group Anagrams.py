a7f5727b063d6f9ffcbe900277f69856f70434d0
Aotmick <justaotz@gmail.com>
2024-05-05 11:57:23 +0600

    bubbleSort,mergeSort updated

--- LeetCode-Easy Solutions/49.Group Anagrams.py
+++ LeetCode-Easy Solutions/49.Group Anagrams.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+<<<<<<< HEAD:LeetCode-Easy Solutions/49.Group Anagrams.py
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -14,3 +15,19 @@ class Solution:
             result[tuple(count)].append(word)
 
         return result.values()
+=======
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result=defaultdict(list)
+        
+        for word in strs:
+            count=[0]*26
+            
+            for char in word:
+                count[ord(char)-ord("a")]+=1
+                
+            result[tuple(count)].append(word)
+            
+        return result.values()
+                
+>>>>>>> 79dd1914627fddd68eba890e0c591b0180d11ae2:LeetCode/49.Group Anagrams.py
